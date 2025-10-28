c278b9928a2287dbf2e4501bdb3c9f80bf0e0319
Daniel Pinna <daniel@pinna.dev.br>
2025-10-28 09:09:23 -0300

    refactor: add ExchangeProxy for to use feignClients

--- src/main/java/br/dev/pinna/proxy/ExchangeProxy.java
+++ src/main/java/br/dev/pinna/proxy/ExchangeProxy.java
@@ -1,4 +1,14 @@
 package br.dev.pinna.proxy;
 
+import br.dev.pinna.dto.Exchange;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
+@FeignClient(name = "exchange-service", url="localhost:8000")
 public interface ExchangeProxy {
+
+    @GetMapping(value = "/exchange-service/{amount}/{from}/{to}")
+    public Exchange getExchange(@PathVariable("amount") Double amount, @PathVariable("from") String from, @PathVariable("to") String to);
 }
