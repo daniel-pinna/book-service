c29f30ca8d70ac63d31beb330ae5c751a28834a0
Daniel Pinna <daniel@pinna.dev.br>
2025-10-28 08:14:21 -0300

    refactor: consume exchange-service with RestTenplate

--- src/main/java/br/dev/pinna/controller/BookController.java
+++ src/main/java/br/dev/pinna/controller/BookController.java
@@ -1,5 +1,6 @@
 package br.dev.pinna.controller;
 
+import br.dev.pinna.dto.Exchange;
 import br.dev.pinna.environment.InstanceInformationService;
 import br.dev.pinna.model.Book;
 import br.dev.pinna.repository.BookRepository;
@@ -9,8 +10,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.Date;
+import java.util.HashMap;
 
 @RestController
 @RequestMapping("book-service")
@@ -28,7 +31,17 @@ public class BookController {
 
         var book = repository.findById(id).orElseThrow();
 
+        HashMap<String, String> params = new HashMap<>();
+        params.put("amount", book.getPrice().toString());
+        params.put("from", "USD");
+        params.put("to", currency);
+
+        var response = new RestTemplate().getForEntity("http://localhost:8000/exchange-service/{amount}/{from}/{to}", Exchange.class, params);
+
+        Exchange exchange = response.getBody();
+
         book.setEnvironment(port);
+        book.setPrice(exchange.getConvertedValue());
         book.setCurrency(currency);
 
         return book;
