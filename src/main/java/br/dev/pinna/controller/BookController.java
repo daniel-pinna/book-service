2495af5ba1a30b18a8fa4929ccbd707919c5da49
Daniel Pinna <daniel@pinna.dev.br>
2025-10-28 09:10:21 -0300

    refactor: Change RestTemplate for FeingClients

--- src/main/java/br/dev/pinna/controller/BookController.java
+++ src/main/java/br/dev/pinna/controller/BookController.java
@@ -3,6 +3,7 @@ package br.dev.pinna.controller;
 import br.dev.pinna.dto.Exchange;
 import br.dev.pinna.environment.InstanceInformationService;
 import br.dev.pinna.model.Book;
+import br.dev.pinna.proxy.ExchangeProxy;
 import br.dev.pinna.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -10,10 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
-import java.util.Date;
-import java.util.HashMap;
 
 @RestController
 @RequestMapping("book-service")
@@ -25,22 +23,18 @@ public class BookController {
     @Autowired
     private BookRepository repository;
 
+    @Autowired
+    ExchangeProxy proxy;
+
     @GetMapping(value = "/{id}/{currency}", produces = MediaType.APPLICATION_JSON_VALUE)
     public Book findBook(@PathVariable("id") Long id, @PathVariable("currency") String currency) {
         String port = informationService.retrieveServerPort();
 
         var book = repository.findById(id).orElseThrow();
 
-        HashMap<String, String> params = new HashMap<>();
-        params.put("amount", book.getPrice().toString());
-        params.put("from", "USD");
-        params.put("to", currency);
+        Exchange exchange = proxy.getExchange(book.getPrice(), "USD", currency);
 
-        var response = new RestTemplate().getForEntity("http://localhost:8000/exchange-service/{amount}/{from}/{to}", Exchange.class, params);
-
-        Exchange exchange = response.getBody();
-
-        book.setEnvironment(port);
+        book.setEnvironment(port + " FEIGN");
         book.setPrice(exchange.getConvertedValue());
         book.setCurrency(currency);
 
